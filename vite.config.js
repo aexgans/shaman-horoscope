71568e9f0a7aeacc8ce71ca5fe045eddf83d00c0
Alexander Iogansen <aexgans@gmail.com>
2026-01-19 23:49:04 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const repositoryName = 'shaman-horoscope'
+
 export default defineConfig({
+   base: process.env.NODE_ENV === 'production' 
+    ? `/shaman-horoscope/`  
+    : '/',               
   plugins: [
     vue(),
     VitePWA({
